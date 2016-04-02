8002963491e4a2731dbc21f773cb7781ec5982d2
alanyao <alanyao@berkeley.edu>
2016-04-02 01:51:52 -0700

    sudoku

--- src/solvers/sudoku_solver.py
+++ src/solvers/sudoku_solver.py
@@ -4,11 +4,16 @@ This file defines the solver for Sudoku.
 
 from solver import Solver
 import time
+from sklearn import datasets, svm, metrics
+from sklearn.externals import joblib
 
 class SudokuSolver(Solver):
     WIDTH = 9
     HEIGHT = 9
     GAME_NAME = "sudoku"
+
+    def __init__(self):
+        self.classifier = joblib.load('../../lib/mnist_svm/svm.pkl')
 
     def detect_and_play(self, board):
         board_repr = self._detect(board)
@@ -18,7 +23,8 @@ class SudokuSolver(Solver):
         return ("sudoku_solution_%s"%board, 0.5, 0.99)
 
     def _detect(self, board):
-        # TODO
+        flat = np.asarray([x for x in row for row in board])
+        data = digits.images.reshape((n_samples, -1))
         pass
 
     def _get_solution(self, board_repr):
