12798e007ee2e29e272f6967eb56f0dbd588eef1
alanyao <alanyao@berkeley.edu>
2016-04-02 03:46:05 -0700

    sudoku

--- src/solvers/sudoku_solver.py
+++ src/solvers/sudoku_solver.py
@@ -7,6 +7,8 @@ import time
 from sklearn import datasets, svm, metrics
 from sklearn.externals import joblib
 import re
+import cv2
+import IPython
 
 class SudokuSolver(Solver):
     WIDTH = 9
@@ -14,18 +16,32 @@ class SudokuSolver(Solver):
     GAME_NAME = "sudoku"
 
     def __init__(self):
-        self.classifier = joblib.load('../../lib/mnist_svm/svm.pkl')
+        self.templates = {}
 
     def detect_and_play(self, board):
         board_repr = self._detect(board)
         board_repr = "000000208920004000000208071036000000000709000000000640860401000000900027209000000"
         solution = self._get_solution(board_repr)
         # TODO: set up return value correctly as specified in solver.py
-        return ("sudoku_solution", 0.5, 0.99)
+        ret = []
+        for idx, c in enumerate(board_repr):
+            if c != solution[idx]: # solution differs
+                im = self._getTemplate(solution[idx])
+                ret.append((self._idx2ij(idx), im))
+                IPython.embed()
+        return (ret, 0.5, 0.99)
+
+    def _getTemplate(self, num):
+        if num not in self.templates:
+            self.templates[num] = cv2.imread('../images/' + num + '.png', -1)[:,:,:4]
+        return self.templates[num]
+
+
+    def _idx2ij(self, idx):
+        idx = int(idx)
+        return idx / 9, idx % 9
 
     def _detect(self, board):
-        flat = np.asarray([x for x in row for row in board])
-        data = digits.images.reshape((n_samples, -1))
         pass
 
     def _get_solution(self, board_repr):
